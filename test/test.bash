b7f5e8b197a91cbe4c8ff73fbbb99fa3a35106d3
Ryusei Fujimura <s23c1125ja@s.chibakoudai.jp>
2024-12-28 00:03:17 +0900

    bash

--- test/test.bash
+++ test/test.bash
@@ -7,6 +7,4 @@ cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-
-cat /tmp/mypkg.log |
-grep 'Listen: 10'
+cat /tmp/mypkg.log | grep -A 10 "September 2025"
