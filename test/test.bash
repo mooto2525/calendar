60425eb5e9cf5405ecd4a51d9faef215977504f4
Ryusei Fujimura <s23c1125ja@s.chibakoudai.jp>
2024-12-31 22:24:43 +0900

    test

--- test/test.bash
+++ test/test.bash
@@ -1,4 +1,13 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Ryusei Fujimura
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
 
 dir=~
 [ "$1" != "" ] && dir="$1"
@@ -6,5 +15,10 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
+
 timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log | grep -A 10 "September 2025"
+[ "$?" = 0 ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
+exit "$res"
