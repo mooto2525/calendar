3efcf09d2d4872565177e375f17aacec0cd490e4
Ryusei Fujimura <s23c1125ja@s.chibakoudai.jp>
2025-01-05 14:20:23 +0900

    test

--- calendar_data/listener.py
+++ calendar_data/listener.py
@@ -5,7 +5,7 @@ from std_msgs.msg import String
 class CalendarListener(Node):
     def __init__(self):
         super().__init__('listener')
-        self.subscription = self.create_subscription(String, 'calender', self.listener_callback, 10)
+        self.subscription = self.create_subscription(String, 'calendar', self.listener_callback, 10)
     def listener_callback(self, msg):
         self.get_logger().info(f'Received calendar data:\n{msg.data}')
 
--- test/test.bash
+++ test/test.bash
@@ -2,21 +2,13 @@
 # SPDX-FileCopyrightText: 2024 Ryusei Fujimura
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-	echo ${1}行目が違うよ
-	res=1
-}
-
-res=0
-
 dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 launch calendar_data talk_listen.launch.py > /tmp/calendar.log
-cat /tmp/calendar.log | grep -A 10 "September 2025"
 
-[ "$res" = 0 ] && echo OK
-exit "$res"
+timeout 10 ros2 launch calendar_data talk_listen.launch.py > /tmp/calendar.log
+
+cat /tmp/calendar.log | grep -A 10 "September 2025"
